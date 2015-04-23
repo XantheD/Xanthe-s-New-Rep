3e6dbd6ff5fe5802608fb7bf023981a2dcee98de
Xanthe <xanthe.dobbie@gmail.com>
2015-04-23 21:21:00 +1000

    Commit assignment to master
    xcode project

---
+++ Assignment/Assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Elizabeth Mitchell on 23/04/2015.
+//  Copyright (c) 2015 Xanthe Dobbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
